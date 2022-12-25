6be6b819a73015169b9641311676e9ee2ec960d2
Serhii <s.saviuk@apiko.com>
2022-12-25 16:46:37 +0200

    add tsx to eslint override

--- src/modules/dashboard/components/dashboard/dashboard.component.tsx
+++ src/modules/dashboard/components/dashboard/dashboard.component.tsx
@@ -1,39 +1,8 @@
 import { useGetLastWeekOrdersStatisticQuery } from '@app/core/types'
 import { Card, CardContent, Typography } from '@mui/material'
-import { Loading, useGetList } from 'react-admin'
+import { Loading } from 'react-admin'
 
 import { FinanceChart } from '../finance-chart/finance-chart.component'
-
-// const data = [
-// 	{
-// 		date: '24/12',
-// 		total: 4000,
-// 	},
-// 	{
-// 		date: '23/12',
-// 		total: 3000,
-// 	},
-// 	{
-// 		date: '22/12',
-// 		total: 2000,
-// 	},
-// 	{
-// 		date: '21/12',
-// 		total: 2780,
-// 	},
-// 	{
-// 		date: '20/12',
-// 		total: 1890,
-// 	},
-// 	{
-// 		date: '19/12',
-// 		total: 2390,
-// 	},
-// 	{
-// 		date: '18/12',
-// 		total: 3490,
-// 	},
-// ].reverse()
 
 export const Dashboard = () => {
 	const { data, loading } = useGetLastWeekOrdersStatisticQuery()
