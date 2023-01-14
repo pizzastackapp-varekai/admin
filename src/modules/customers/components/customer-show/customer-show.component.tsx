f4a9f928d1bf40ebb63b4c7e4b43beab37b9552f
Serhii <s.saviuk@apiko.com>
2023-01-14 22:15:04 +0200

    add orders to customer

---
+++ src/modules/customers/components/customer-show/customer-show.component.tsx
@@ -0,0 +1,46 @@
+import { CustomTitle } from '@app/common/components/custom-title/custom-title.component'
+import { Orders } from '@app/core/types'
+import {
+	Datagrid,
+	DateField,
+	FunctionField,
+	ReferenceManyField,
+	Show,
+	SimpleShowLayout,
+	TextField,
+	useRecordContext,
+} from 'react-admin'
+
+export const CustomerShow = () => {
+	const record = useRecordContext()
+	return (
+		<Show title={<CustomTitle source="name" />}>
+			<SimpleShowLayout>
+				<TextField source="address" />
+				<TextField source="name" />
+				<TextField source="phone" />
+				<ReferenceManyField
+					target="client_phone"
+					reference="orders"
+					source="phone"
+					label="Замовлення"
+					sort={{ field: 'created_at', order: 'DESC' }}
+				>
+					<Datagrid rowClick="show">
+						<DateField
+							source="created_at"
+							label="Дата"
+							showTime
+							options={{ timeZone: 'Europe/Kiev' }}
+						/>
+						<FunctionField
+							source="sum"
+							label="Сума"
+							render={(record: Orders) => `${record.sum} грн.`}
+						/>
+					</Datagrid>
+				</ReferenceManyField>
+			</SimpleShowLayout>
+		</Show>
+	)
+}
--- src/modules/customers/customers.resource.ts
+++ src/modules/customers/customers.resource.ts
@@ -1,9 +1,11 @@
 import PersonIcon from '@mui/icons-material/Person'
-import { CustomerList } from './components/customer-list.component'
+import { CustomerList } from './components/customer-list/customer-list.component'
+import { CustomerShow } from './components/customer-show/customer-show.component'
 
 export const CustomerResource = {
 	name: 'customers',
 	list: CustomerList,
+	show: CustomerShow,
 	options: {
 		label: 'Користувачі',
 	},
